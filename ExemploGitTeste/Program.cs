e1c13b7d29d853e8099845eaaba3ac56cb263ef2
Davi Ruhan de Oliveira <davi_ruhan@hotmail.com>
2020-06-02 22:13:27 -0300

    Primeiro commit

---
+++ ExemploGitTeste/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExemploGitTeste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
